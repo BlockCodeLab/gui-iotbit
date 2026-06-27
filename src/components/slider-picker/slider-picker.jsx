172a4dc179b9a8165c84bd22da98c880ea43562b
Yeqin Gong <yeqin.gong@qq.com>
2026-06-27 16:24:08 +0800

    增加数值滑动控件

---
+++ src/components/slider-picker/slider-picker.jsx
@@ -0,0 +1,33 @@
+import { useCallback } from 'preact/hooks';
+import { classNames } from '@blockcode/utils';
+import { Tooltip } from '@blockcode/core';
+import styles from './slider-picker.module.css';
+
+export function SliderPicker({ min, max, step, value, children, onChange }) {
+  const handleChange = useCallback((e) => {
+    onChange?.(e.target.value);
+  }, []);
+
+  return (
+    <Tooltip
+      clickable
+      placement="bottom"
+      className={styles.pickerTooltip}
+      content={
+        <div className="scratchSliderDiv">
+          <input
+            type="range"
+            className={classNames('scratchFieldSlider', styles.slider)}
+            min={min ?? 0}
+            max={max ?? 100}
+            step={step ?? 1}
+            value={value ?? min ?? 0}
+            onInput={handleChange}
+          />
+        </div>
+      }
+    >
+      {children}
+    </Tooltip>
+  );
+}
