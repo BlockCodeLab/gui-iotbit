e95c6ed24bf3f0283d560f0297c8b4365f557daf
Yeqin Gong <yeqin.gong@qq.com>
2026-07-25 15:12:00 +0800

    优化BLE下载和增加连接提示

--- src/components/device-menu/device-menu.jsx
+++ src/components/device-menu/device-menu.jsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect } from 'preact/hooks';
 import { useComputed } from '@preact/signals';
 import { classNames, sleepMs, nanoid } from '@blockcode/utils';
-import { useAppContext, useProjectContext, setAlert, setAppState, translate, logger } from '@blockcode/core';
+import { useAppContext, useProjectContext, setAlert, delAlert, setAppState, translate, logger } from '@blockcode/core';
 import { MPYUtils } from '@blockcode/board';
 import { downloadProgram } from '../../lib/download-program';
 import deviceFilters from './device-filters.yaml';
@@ -11,12 +11,14 @@ import { FirmwareSection } from './firmware-section';
 import styles from './device-menu.module.css';
 
 const errorAlert = (err, id) => {
-  if (err.name === 'NotFoundError') return;
-  if (err.name === 'NetworkError') {
+  if (err.name === 'NotFoundError') {
+    delAlert(id);
+  } else if (err.name === 'NetworkError') {
     setAlert('connectionBusy', { id }, 2000);
   } else {
     setAlert('connectionError', { id }, 2000);
   }
+  setAppState('deviceAlertId', null);
 };
 
 export function DeviceMenu({ itemClassName }) {
@@ -26,51 +28,67 @@ export function DeviceMenu({ itemClassName }) {
 
   const device = useComputed(() => appState.value?.device);
 
+  const lastBLEDevice = useComputed(() => appState.value?.lastBLEDevice);
+
   const deviceAlertId = useComputed(() => appState.value?.deviceAlertId);
 
   const connectDevice = useCallback(async (newDevice) => {
-    if (newDevice === device.value) return;
-    await device.value?.disconnect();
-    await sleepMs(500);
+    if (newDevice === device.value && newDevice.type !== 'ble') return;
+    if (newDevice.type !== 'ble') {
+      await device.value?.disconnect();
+      await sleepMs(500);
+    }
     const handleConnect = () => connectDevice(newDevice);
     const handleDisconnect = (err) => {
       if (err) {
         errorAlert(err, deviceAlertId.value);
         logger.warn(translate('gui.logs.disconnected', 'Device disconnected') + ': ' + err.message);
+        setAppState('lastBLEDevice', null);
       }
-      setAppState('device', null);
-      setAppState('deviceAlertId', null);
+      setAppState({
+        device: null,
+        deviceAlertId: null,
+      });
       newDevice.off('connect', handleConnect);
       newDevice.off('disconnect', handleDisconnect);
     };
     newDevice.on('connect', handleConnect);
     newDevice.on('disconnect', handleDisconnect);
-    setAppState('device', newDevice);
+    setAppState({
+      device: newDevice,
+      deviceAlertId: null,
+    });
   }, []);
 
   const handleConnectUSB = useCallback(async () => {
     if (deviceAlertId.value) return;
+    const alertId = setAlert('connecting');
+    setAppState('deviceAlertId', alertId);
     try {
       const newDevice = await MPYUtils.connect(deviceFilters, {
         baudRate: 115200,
       });
       connectDevice(newDevice);
-      setAlert('connected', 1000);
+      setAlert('connected', { id: alertId }, 1000);
       logger.success(translate('gui.logs.connectedType', 'Device connected with {type}', { type: 'USB' }));
+      setAppState('lastBLEDevice', null);
     } catch (err) {
-      errorAlert(err, deviceAlertId.value);
+      errorAlert(err, alertId);
     }
   }, []);
 
   const handleConnectBLE = useCallback(async () => {
     if (deviceAlertId.value) return;
+    const alertId = setAlert('connecting');
+    setAppState('deviceAlertId', alertId);
     try {
       const newDevice = await MPYUtils.connectBLE();
       connectDevice(newDevice);
-      setAlert('connected', 1000);
+      setAlert('connected', { id: alertId }, 1000);
       logger.success(translate('gui.logs.connectedType', 'Device connected with {type}', { type: 'BLE' }));
+      setAppState('lastBLEDevice', null);
     } catch (err) {
-      errorAlert(err, deviceAlertId.value);
+      errorAlert(err, alertId);
     }
   }, []);
 
@@ -79,7 +97,10 @@ export function DeviceMenu({ itemClassName }) {
     if (deviceAlertId.value) return;
 
     const alertId = nanoid();
-    setAppState('deviceAlertId', alertId);
+    setAppState({
+      deviceAlertId: alertId,
+      lastBLEDevice: device.value.type === 'ble' ? device.value : null,
+    });
     await downloadProgram(alertId, device.value, file.value, assets.value);
 
     setAppState('deviceAlertId', null);
@@ -88,17 +109,35 @@ export function DeviceMenu({ itemClassName }) {
   const handleDisconnect = useCallback(() => {
     if (deviceAlertId.value) return;
     device.value?.disconnect();
+    setAppState('lastBLEDevice', null);
     logger.warn(translate('gui.logs.disconnected', 'Device disconnected'));
   }, []);
 
   const handleReset = useCallback(() => {
+    if (!device.value) return;
     if (deviceAlertId.value) return;
-    device.value?.reset();
+    if (device.value.type === 'ble') {
+      setAppState('lastBLEDevice', device.value);
+    }
+    device.value.reset();
     setAlert('reseting', 1000);
   }, []);
 
-  useEffect(() => {
-    if (deviceAlertId.value && !device.value) {
+  useEffect(async () => {
+    if (device.value) return;
+    if (lastBLEDevice.value && lastBLEDevice.value.type === 'ble' && !lastBLEDevice.value.connected) {
+      const alertId = setAlert('reconnecting');
+      setAppState('deviceAlertId', alertId);
+      try {
+        await lastBLEDevice.value.connect();
+        await connectDevice(lastBLEDevice.value);
+        logger.success(translate('gui.logs.reconnected', 'Device reconnected'));
+      } catch (err) {
+        errorAlert(err, alertId);
+      }
+      delAlert(alertId);
+    }
+    if (deviceAlertId.value) {
       setAppState('deviceAlertId', null);
     }
   }, [device.value]);
@@ -155,7 +194,6 @@ export function DeviceMenu({ itemClassName }) {
               onClick={handleConnectUSB}
             />
             <MenuItem
-              disabled
               className={classNames(itemClassName, styles.blankCheckItem)}
               label={
                 <Text
